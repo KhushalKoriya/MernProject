8f145f09e825e414925f8c58bb305aef7525ff96
khushal.k <khushal.k@covrize.com>
2023-02-06 17:01:40 +0530

    add module

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { registerUser } from '../controllers/userControllers.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 //Authentication
 router.post('/Register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
